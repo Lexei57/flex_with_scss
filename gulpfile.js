b57574093450027cd589d860a8dd249102549aa0
Lexei <mnzllexei@gmail.com>
2022-05-20 15:04:04 +0300

    ee

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('css', () => {
-    return gulp.src('./scss/**/style.scss')
+    return gulp.src('./scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./css'));
 })
 
-gulp.watch('./scss/**/style.scss', gulp.series('css'))
+gulp.watch('./scss/**/*.scss', gulp.series('css'))
 
 gulp.task("start", gulp.series('css'))
